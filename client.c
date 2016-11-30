7a1a476038bb530f27c7d77d37164e62bf671a99
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-11-30 22:08:11 +0530

    useless files removed + code cleanup

--- client.c
+++ client.c
@@ -4,78 +4,37 @@
 #include<sys/stat.h>
 
 int main(){
-	int fdw, fdr, user_input, message, choice;
-	// mkfifo("FIFO1",0600);
+	int fd, user_input, message, choice;
+	fd = open("FIFO1", O_RDWR);
 
-	// printf("No Client connected to recieve!\n");
-	fdw = open("FIFO1", O_RDWR);
-
-	// fdw = open("FIFO1", O_WRONLY);
-	// fdr = open("FIFO1", O_RDONLY);
-
-	// printf("\nA Client connected to recieve messages!\n");
-	// printf("-- Enter '-1' to exit --\n");
-
-	// do{
-	// 	printf("1 : READ\n");
-	// 	printf("2 : WRITE\n");
-	// 	// printf("3 : EXIT\n");
-	// 	printf("Enter choice: ");
+	do{
+		printf("1 : READ\n");
+		printf("2 : WRITE\n");
+		printf("Enter choice: ");
 		scanf("%d", &choice);
 
-	// 	switch(choice){
-	// 		case 1:
-	// 			while(1){	
-	// 				if(read(fdw, &message, sizeof(int)) != 0){
-	// 					if(message % 10 == 2){
-	// 						message = message / 10;
-	// 						printf("Message recieved : %d\n", message);
-	// 						int output = 12;
-	// 						write(fdw, &output, sizeof(int));
-	// 					}
-	// 				// } else {
-	// 					// printf("Buffer Empty!\n");
-	// 					break;
-	// 				}
-	// 			}
-	// 			break;
-	// 		case 2:
-			while(1){
-				user_input = 1;
-				write(fdw, &choice, sizeof(int));
-			}
-				// printf("Enter the message to be sent :");
-				// scanf("%d", &user_input);
-				// 				printf("Successfully read\n\n");
+		switch(choice){
+			case 1:
+				while(1){	
+					if(read(fd, &message, sizeof(int)) != 0){
+						if(message % 10 == 2){
+							message = message / 10;
+							printf("Message recieved : %d\n", message);
+							break;
+						}
+					}
+				}
+				break;
+			case 2:
+				printf("Enter the message to be sent :");
+				scanf("%d", &user_input);
+				write(fd, &user_input, sizeof(int));
+								printf("Successfully read\n");
+				break;
+		}
+		usleep(1000);
+	} while(1);
 
-				// while(1){
-				// 	int output;
-				// 	if(read(fdw, &output, sizeof(int)) != 0){
-				// 		if(output % 10 == 2){
-				// 			output = output / 10;
-				// 			if(output == 1){
-				// 				printf("Successfully read\n\n");
-				// 			} else {
-				// 				printf("Some error\n");
-				// 			}
-				// 			break;
-				// 		}
-				// 	}
-				// }
-		// 		break;
-		// 	default:
-		// 		break;
-		// }
-
-
-		
-		// if(user_input == -1)
-		// 	break;
-
-	// 	usleep(1000);
-	// } while(1);
-
-	close(fdw);
-	// close(fdr);
+	close(fd);
 	return 0;
 }
--- server.c
+++ server.c
@@ -4,29 +4,21 @@
 #include<sys/stat.h>
 
 int main(){
-	int fdw, fdr, user_input, message, choice;
+	int fd, message;
 	mkfifo("FIFO1",0600);
 
-	fdw = open("FIFO1", O_RDWR);
-	// fdr = open("FIFO1", O_RDONLY);
+	fd = open("FIFO1", O_RDWR);
 
-	// printf("\nA Client connected to recieve messages!\n");
-	// printf("-- Enter '-1' to exit --\n");
-
-	// while(1){
-		while(read(fdw, &message, sizeof(int)) != 0){
-			// if(message % 10 == 1){
-				// message = message / 10;
+	while(1){
+		if(read(fd, &message, sizeof(int)) != 0){
+			if(message % 10 == 1){
+				message = message / 10;
 				printf("Message recieved : %d\n", message);
-				// int output = 12;
-				// write(fdw, &output, sizeof(int));
-		// 	}
-		// }
-	
-		// usleep(1000);
+			}
+		}
+		usleep(1000);
 	}
 
-	close(fdw);
-	// close(fdr);
+	close(fd);
 	return 0;
 }
